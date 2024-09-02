57b8afc452378651f63c1603c6370263754fb934
Ramon Pacheco <ramonhppacheco@gmail.com>
2024-09-02 12:03:03 +1000

    Implemented tasks 1 and 2 for Module 2

---
+++ Module 2/consecutiveSequence.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+
+public class consecutiveSequence {
+    static int longestSeq = 0;
+
+    public static void main(String[] args) {
+        int[] X = { 8, 3, 2, 5, 10, 9, 7, 6, 11 };
+        System.out.println("Start point: " + Arrays.toString(X));
+        QuickSort(X, 0, X.length - 1);
+        countSeq(X);
+        System.out.println("Final result: " + Arrays.toString(X));
+        System.out.println("Longest sequence: " + longestSeq);
+    }
+
+    static void QuickSort(int[] list, int startIndex, int endIndex) {
+        if (startIndex < endIndex) {
+            int pivotIndex = partition(list, startIndex, endIndex);
+            QuickSort(list, startIndex, pivotIndex - 1);
+            QuickSort(list, pivotIndex + 1, endIndex);
+        }
+    }
+
+    static int partition(int[] list, int startIndex, int endIndex) {
+        int pivotVal = list[startIndex];
+        int leftMark = startIndex + 1;
+        int rightMark = endIndex;
+
+        while (leftMark <= rightMark) {
+            while (leftMark <= rightMark && list[leftMark] <= pivotVal)
+                leftMark++;
+            while (leftMark <= rightMark && list[rightMark] >= pivotVal)
+                rightMark--;
+
+            if (leftMark <= rightMark)
+                swap(list, leftMark, rightMark);
+        }
+        swap(list, startIndex, rightMark);
+        return rightMark;
+    }
+
+    static void swap(int[] list, int i, int j) {
+        int temp = list[i];
+        list[i] = list[j];
+        list[j] = temp;
+    }
+
+    static void countSeq(int[] list) {
+        int currentSeq = 1;
+        for (int i = 0; i < list.length - 1; i++) {
+            if (list[i] + 1 == list[i + 1]) {
+                currentSeq++;
+                System.out.println("currentSeq");
+            } else {
+                currentSeq = 1;
+            }
+            longestSeq = currentSeq > longestSeq ? currentSeq : longestSeq;
+        }
+    }
+
+}
---
+++ Module 2/pivotPosition.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+
+public class pivotPosition {
+
+    public static void main(String[] args) {
+        int[] testList = { 54, 26, 93, 17, 77, 31, 44, 55, 20 };
+        System.out.println("Start point: " + Arrays.toString(testList));
+
+        long startTime = System.currentTimeMillis();
+        QuickSort(testList, 0, testList.length - 1);
+        long endTime = System.currentTimeMillis();
+
+        long delta = endTime - startTime;
+
+        System.out.println("Quick sort execution time: " + delta);
+        System.out.println("Final result: " + Arrays.toString(testList));
+    }
+
+    static void QuickSort(int[] list, int startIndex, int endIndex) {
+
+        if (startIndex < endIndex) {
+            swapPivot(list, startIndex, endIndex);
+            int pivotIndex = partition(list, startIndex, endIndex);
+            QuickSort(list, startIndex, pivotIndex - 1);
+            QuickSort(list, pivotIndex + 1, endIndex);
+        }
+    }
+
+    static void swapPivot(int[] list, int startIndex, int endIndex) {
+        int midIndex = (startIndex + endIndex) / 2 + 1;
+        if (list[startIndex] <= list[midIndex] && list[midIndex] <= list[endIndex]) {
+            swap(list, startIndex, midIndex);
+        } else if (list[startIndex] <= list[endIndex] && list[midIndex] <= list[endIndex]) {
+            swap(list, startIndex, endIndex);
+        }
+    }
+
+    static int partition(int[] list, int startIndex, int endIndex) {
+        int pivotVal = list[startIndex];
+
+        int leftMark = startIndex + 1;
+        int rightMark = endIndex;
+
+        while (leftMark <= rightMark) {
+            while (leftMark <= rightMark && list[leftMark] <= pivotVal)
+                leftMark++;
+            while (leftMark <= rightMark && list[rightMark] >= pivotVal)
+                rightMark--;
+
+            if (leftMark <= rightMark)
+                swap(list, leftMark, rightMark);
+        }
+        swap(list, startIndex, rightMark);
+        return rightMark;
+    }
+
+    static void swap(int[] list, int i, int j) {
+        int temp = list[i];
+        list[i] = list[j];
+        list[j] = temp;
+    }
+
+}
