cda4bd26e7539c0b3a595c4428310058e70e033f
Ramon Pacheco <ramonhppacheco@gmail.com>
2024-09-16 12:23:00 +1000

    End of Week 4 tutorial

---
+++ Module 4/Substring.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Substring {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = scanner.nextLine();
+        scanner.close();
+
+        // String str = new String("abcdefa");
+        String currentStr = new String("");
+        String longestStr = new String("");
+        char prev = str.charAt(0);
+        for (int i = 0; i < str.length() - 1; i++) {
+
+            if (str.charAt(i) > prev) {
+                currentStr += str.charAt(i);
+            } else {
+                currentStr = str.substring(i, i + 1);
+            }
+            prev = str.charAt(i);
+            longestStr = currentStr.length() > longestStr.length() ? currentStr : longestStr;
+        }
+        System.err.println(longestStr);
+    }
+}
